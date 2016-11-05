4aca84b8309c2fd6ea8f622bffe6575cd9c0f0f7
Chris C Cerami <chrisccerami@gmail.com>
2016-11-05 02:02:11 -0400

    Click next outgoing message to send it

--- app/message/model.js
+++ app/message/model.js
@@ -1,8 +1,11 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   content: DS.attr('string'),
   incoming: DS.attr('boolean'),
   goTo: DS.attr('number'),
-  sleepTime: DS.attr('number')
+  sleepTime: DS.attr('number'),
+
+  outgoing: Ember.computed.not('incoming')
 });
